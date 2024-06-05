c8307c3fee7a794f2ede90a730e25656c8fac165
Amr Magdy <amagdy195@yahoo.com>
2024-06-05 22:17:16 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Amr Magdy on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
